c9fb7627a9c60aa31b9daeab835185b09d33460e
Manuela Miyama <manueladmonteirodossantos@gmail.com>
2021-02-15 10:51:19 -0300

    procedures & triggers para updated_at em users

--- migrations/20210215083720_create_table_projects.js
+++ migrations/20210215083720_create_table_projects.js
@@ -1,4 +1,6 @@
-exports.up = knex =>
+const { onUpdateTrigger } = require('../knexfile');
+
+exports.up = async knex =>
 knex.schema.createTable('projects', table => {
     table.increments('id');
     table.text('title');
@@ -6,7 +8,7 @@ knex.schema.createTable('projects', table => {
     table.integer('user_id').references('users.id').notNullable().onDelete('CASCADE');
     
     table.timestamps(true, true)
-});
+}).then(() => knex.raw(onUpdateTrigger('projects')));
 
-exports.down = knex => knex.schema.dropTable('projects');
+exports.down = async knex => knex.schema.dropTable('projects');
 
--- migrations/20210215093954_add_column_deleted_at_to_users.js
+++ migrations/20210215093954_add_column_deleted_at_to_users.js
@@ -4,6 +4,6 @@ exports.up = knex => knex.schema.alterTable('users', table => {
     table.timestamp('deleted_at');
 });
 
-exports.down = knex => knex.schema.alterTable('users', knex => {
+exports.down = knex => knex.schema.alterTable('users', table => {
     table.dropColumn('deleted_at');
 });
